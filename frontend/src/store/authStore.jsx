7c4b12495a1b2969b2341f2dd91e83a61171796e
Suyash Parkash <suyash@Suyashs-MacBook-Air.local>
2025-07-25 00:09:18 +0530

    Fix signup bug and update auth logic

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5050/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 // ✅ Always send cookies with each request
 axios.defaults.withCredentials = true;
